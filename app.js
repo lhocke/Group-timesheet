95700df5fc20c760a40a1554af9d19f826e1207b
Chi <chulinguy@gmail.com>
2017-06-27 13:19:36 -0700

    added Firebase stuff

---
+++ app.js
@@ -0,0 +1,11 @@
+
+ // Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyC6nW2C9TAow2BfXRZrv9I0XFO9d2Nbu9A",
+   authDomain: "group-project-employee-data.firebaseapp.com",
+   databaseURL: "https://group-project-employee-data.firebaseio.com",
+   projectId: "group-project-employee-data",
+   storageBucket: "",
+   messagingSenderId: "777829654096"
+ };
+ firebase.initializeApp(config);
