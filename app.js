69b6979b345bb81e2b0c5ef1cdec0bddd53e0876
Chi <chulinguy@gmail.com>
2017-06-27 13:31:43 -0700

    starting app.js

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC6nW2C9TAow2BfXRZrv9I0XFO9d2Nbu9A",
+  authDomain: "group-project-employee-data.firebaseapp.com",
+  databaseURL: "https://group-project-employee-data.firebaseio.com",
+  projectId: "group-project-employee-data",
+  storageBucket: "",
+  messagingSenderId: "777829654096"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
- // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyC6nW2C9TAow2BfXRZrv9I0XFO9d2Nbu9A",
-   authDomain: "group-project-employee-data.firebaseapp.com",
-   databaseURL: "https://group-project-employee-data.firebaseio.com",
-   projectId: "group-project-employee-data",
-   storageBucket: "",
-   messagingSenderId: "777829654096"
- };
- firebase.initializeApp(config);
+//extract new employ info
+$('#sumbit-button').on('click', function(){
+  var newName = $('#emoloyee-name').val().trim();
+  var newRole = $('#role').val().trim();
+  var newStartDate = $('#start-date').val().trim();
+  var newRate = $('#monthly-rate').val().trim();
+
+  database.ref().push({
+    name: newName,
+    role: newRole,
+    rate: newRate,
+    date: newStartDate,
+    
+  })
+})
+
+
